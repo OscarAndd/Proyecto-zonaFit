8ebc20b049f3322128eae14da3405aa3fbfdf5d8
OscarAndd <oaalvarezp@hotmail.com>
2025-02-23 22:07:34 -0500

    cambio de carpeta

--- ZonaFitSpring/src/main/java/ZonaFit/ZonaFitSpring/repositorio/ClienteRepositorio.java
+++ ZonaFitSpring/src/main/java/ZonaFit/ZonaFitSpring/repositorio/ClienteRepositorio.java
@@ -2,7 +2,10 @@ package ZonaFit.ZonaFitSpring.repositorio;
 
 import ZonaFit.ZonaFitSpring.modelo.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//ESTA INTERFAZ EXTENDERA DE LA INTERFAZ JpaRepository LA CUAL CONTIENE LOS METODOS CRUD
+// NECESARIOS PARA EL FUNCIONAMIENTO. SpringBoot LO HACE DE MANERA AUTOMATICA Y
+//LOS GENERICOS QUE RECIBE SON Cliente: CLASE MARCADA CON @entity O LA CLASE DE LOS DATOS/MODELO Y ADEMAS
+//RECIBE EL TIPO DE DATO DE LA LLAVE PRIMARIA DE LA MISMA CLASE.
 public interface ClienteRepositorio extends JpaRepository <Cliente,Integer> {
     //AL SER DE UN GENERICO JpaRepository SE DEBE PROPORCIONAR LA CLASE QUE CONTIENE
     //EL ORM=OBJECT RELATIONAL MAPPING Y EL TIPO DE DATO DE LA LLAVE PRIMARIA
